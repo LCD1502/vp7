e7321cefc32828782d38d4338043aa230f6afbfb
Hiệp Nguyễn Hoàng <19520536@gm.uit.edu.vn>
2022-04-15 12:31:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(morgan('dev'));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
 
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World frontend');
 });
 
 app.use('/api/v1/user', userRoutes);

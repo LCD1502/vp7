80445937b3f12aa55cb0337555f665e8a810f95b
Hiệp Nguyễn Hoàng <19520536@gm.uit.edu.vn>
2022-04-22 15:44:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.all('/', function(req, res, next) {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World Dung qua ngu theem cdn');
+    res.send('Hello World Dung qua ngu theem cdn 2.0');
 });
 
 app.use('/api/v1/user', userRoutes);

146ce39ac6b675a082a17245d1b21750ca970457
Hiệp Nguyễn Hoàng <19520536@gm.uit.edu.vn>
2022-06-01 23:44:06 +0700

    add getOneShowroom

--- routes/showRoomRoutes.js
+++ routes/showRoomRoutes.js
@@ -10,6 +10,7 @@ router.use((req, res, next) => {
 });
 
 router.get('/', showRoomControllers.getAllShowRooms);
+router.get('/:showRoomId', showRoomControllers.getOneShowRoom);
 router.post(
     '/',
     authControllers.protect,
